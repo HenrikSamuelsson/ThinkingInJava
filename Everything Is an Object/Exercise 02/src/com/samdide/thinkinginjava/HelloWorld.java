61154bbd04e0578d96da59c5910f268877d75644
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-05-01 20:11:45 +0200

    Commit of exercise 2.

---
+++ Everything Is an Object/Exercise 02/src/com/samdide/thinkinginjava/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.samdide.thinkinginjava;
+
+/**
+ * Displays a string.
+ * @author Henrik Samuelsson
+ */
+public class HelloWorld {
+	
+	/**
+	 * Entry point to class and application.
+	 * @param args array of string arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
