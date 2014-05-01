9ce99c10ae96795f4216fead6687a51f55ce610f
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-05-01 21:13:10 +0200

    Committing exercise 4.

---
+++ Everything Is an Object/Exercise 04/src/com/samdide/thinkinginjava/Main.java
@@ -0,0 +1,18 @@
+package com.samdide.thinkinginjava;
+
+/**
+ * Creation of an object and assigning of some values.
+ * @author Henrik Samuelsson
+ */
+public class Main {
+	/**
+	 * Entry point to class and application.
+	 * @param args array of string arguments
+	 */
+	public static void main(String[] args) {
+		DataOnly data = new DataOnly();
+		data.i = 47;
+		data.d = 1.1;
+		data.b = false;
+	}
+}
