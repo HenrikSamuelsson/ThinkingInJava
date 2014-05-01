300fbc3500477256be6bb3d32afb380567b59f46
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-05-01 20:32:33 +0200

    Committing exercise 3.

---
+++ Everything Is an Object/Exercise 03/src/com/samdide/thinkinginjava/ATypeName.java
@@ -0,0 +1,9 @@
+package com.samdide.thinkinginjava;
+
+/**
+ * A class that you can not really do anything with.
+ * @author Henrik Samuelsson
+ */
+public class ATypeName {
+	/* Class body goes here */
+}
---
+++ Everything Is an Object/Exercise 03/src/com/samdide/thinkinginjava/Main.java
@@ -0,0 +1,17 @@
+package com.samdide.thinkinginjava;
+
+/**
+ * Creates a useless object.
+ * @author Henrik Samuelsson
+ */
+public class Main {
+	
+	/**
+	 * Entry point to class and application.
+	 * @param args array of string arguments
+	 */
+	public static void main(String[] args) {
+		ATypeName a = new ATypeName();
+		System.out.println("Object created!");
+	}
+}
