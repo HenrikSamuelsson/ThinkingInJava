f6960f2d64816bf9b29088fe0e59b0ee6e3f0386
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-05-03 16:32:43 +0200

    Committing exercise 1

---
+++ Operators/Exercise 1/src/com/samdide/thinkinginjava/Main.java
@@ -0,0 +1,24 @@
+package com.samdide.thinkinginjava;
+
+import static net.mindview.util.Print.*;
+
+/**
+ * Shows how to display text in two different ways.
+ * 
+ * @author Henrik Samuelsson
+ * 
+ */
+
+public class Main {
+	/**
+	 * Entry point to class and application.
+	 * 
+	 * @param args
+	 *            array of string arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Printing using the ordinary version.");
+		print("Printing using the short version.");
+	}
+
+}
