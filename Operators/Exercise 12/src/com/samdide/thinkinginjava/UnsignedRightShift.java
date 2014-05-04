6cc4a6600a52cc82cfacd2775b723b80bd7d7bdd
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-05-04 20:00:03 +0200

    Committing exercise 12.

---
+++ Operators/Exercise 12/src/com/samdide/thinkinginjava/UnsignedRightShift.java
@@ -0,0 +1,88 @@
+package com.samdide.thinkinginjava;
+
+/**
+ * Repeated unsigned right shift of an integer.
+ * <p>
+ * The code could obviously have been made shorter by the use of a loop but
+ * this concept has not been introduced yet in the book where this exercise
+ * comes from. Hence the brute force approach.
+ * 
+ * @author Henrik Samuelsson
+ */
+public class UnsignedRightShift {
+	/**
+	 * Entry point to class and application.
+	 * 
+	 * @param args
+	 *            array of string arguments
+	 */
+	static public void main(String[] args) {
+		int i = 0xFFFFFFFF;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i << 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+		i = i >>> 1;
+		System.out.println(Integer.toBinaryString(i) + " (" + i + ")");
+	}
+
+}
