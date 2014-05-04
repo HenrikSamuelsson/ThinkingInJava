77ead9277df71c5a9e1181c6a7e5d97598520a9d
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-05-04 20:09:17 +0200

    Committing exercise 13.

---
+++ Operators/Exercise 13/src/com/samdide/thinkinginjava/CharToBinary.java
@@ -0,0 +1,21 @@
+package com.samdide.thinkinginjava;
+
+/**
+ * Displays some characters in binary form.
+ * 
+ * @author Henrik Samuelsson
+ */
+public class CharToBinary {
+	/**
+	 * Entry point to class and application.
+	 * 
+	 * @param args
+	 *            array of string arguments
+	 */
+	static public void main(String[] args) {
+		System.out.println(Integer.toBinaryString('J'));
+		System.out.println(Integer.toBinaryString('a'));
+		System.out.println(Integer.toBinaryString('v'));
+		System.out.println(Integer.toBinaryString('a'));
+	}
+}
