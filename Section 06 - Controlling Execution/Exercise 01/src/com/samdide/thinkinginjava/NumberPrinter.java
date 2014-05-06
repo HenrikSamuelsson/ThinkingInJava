69c9cb2329ac239cf01c81eb0d0ac945b6b168cf
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-05-06 22:51:22 +0200

    Committing exercise 1.

---
+++ Section 06 - Controlling Execution/Exercise 01/src/com/samdide/thinkinginjava/NumberPrinter.java
@@ -0,0 +1,22 @@
+package com.samdide.thinkinginjava;
+
+/**
+ * Prints values from 1 to 100.
+ * 
+ * @author Henrik Samuelsson
+ */
+public class NumberPrinter {
+	/**
+	 * Entry point to class and application.
+	 * 
+	 * @param args
+	 *            array of string arguments
+	 */
+	public static void main(String[] args) {
+		for(int i = 1; i <= 100; i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
