1fdfb9a837ee858d6ff05996dcac049642302ffa
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-05-18 22:28:57 +0200

    Committing exercise 3.

---
+++ Section 07 - Initilization and Cleanup/Exercise 03/src/com/samdide/thinkinginjava/MessagePrint.java
@@ -0,0 +1,17 @@
+package com.samdide.thinkinginjava;
+
+/**
+ * Class that prints a message on construction.
+ * 
+ * @author Henrik Samuelsson
+ */
+public class MessagePrint {
+	
+	/**
+	 * Constructs an object and prints a message.
+	 */
+	MessagePrint() {
+		System.out.println("I print messages on contstruction!");
+	}
+	
+}
