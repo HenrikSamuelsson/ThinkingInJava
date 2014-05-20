df12a762b5d36a7b5c3f967979e99a24060369a6
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-05-20 22:38:33 +0200

    Committing exercise 7.

---
+++ Section 07 - Initilization and Cleanup/Exercise 07/src/com/samdide/thinkinginjava/DefaultConstructorClass.java
@@ -0,0 +1,12 @@
+package com.samdide.thinkinginjava;
+
+/**
+ * Dummy class used to demonstrate that a default constructor is automatically
+ * synthesized.
+ * 
+ * @author Henrik Samuelsson
+ */
+
+public class DefaultConstructorClass {
+	// Intentionally left blank.
+}
---
+++ Section 07 - Initilization and Cleanup/Exercise 07/src/com/samdide/thinkinginjava/Main.java
@@ -0,0 +1,18 @@
+package com.samdide.thinkinginjava;
+
+/**
+ * Test program for the DefaultConstructorClass.
+ * 
+ * @author Henrik Samuelsson
+ */
+public class Main {
+	/**
+	 * Entry point to application.
+	 * 
+	 * @param args
+	 *            array of string arguments
+	 */
+	public static void main(String[] args) {
+		new DefaultConstructorClass();
+	}
+}
