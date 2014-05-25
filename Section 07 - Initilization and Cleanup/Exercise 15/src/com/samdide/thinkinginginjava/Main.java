85b1fb9394dc3441ba7fd5249914c4418e123ecc
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-05-25 14:56:22 +0200

    Committing exercise 15.

---
+++ Section 07 - Initilization and Cleanup/Exercise 15/src/com/samdide/thinkinginginjava/Main.java
@@ -0,0 +1,23 @@
+package com.samdide.thinkinginginjava;
+
+/**
+ * Test program for the StringHolder class.
+ * 
+ * @author Henrik Samuelsson
+ */
+public class Main {
+	
+	/**
+	 * Entry point to application.
+	 * 
+	 * @param args
+	 *            array of string arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Inside main() 1.");
+		new StringHolder();
+		System.out.println("Inside main() 2.");
+		new StringHolder("Hello!");
+	}
+	
+}
---
+++ Section 07 - Initilization and Cleanup/Exercise 15/src/com/samdide/thinkinginginjava/StringHolder.java
@@ -0,0 +1,36 @@
+package com.samdide.thinkinginginjava;
+
+/**
+ * Class that holds a string that illustrates "instance initialization".
+ * <p>
+ * The instance initialization block is always called regardless of what
+ * constructor that is used, s is in this way guaranteed to have been set.
+ * 
+ * @author Henrik Samuelsson
+ */
+public class StringHolder {
+	String s;
+	{
+		s = "instance initialization";
+		System.out.println("Inside instance initialization block.");
+	}
+
+	/**
+	 * Constructor that does not take any arguments.
+	 */
+	public StringHolder() {
+		System.out.println("Constructor without arguments, s = " + s + ".");
+	}
+
+	/**
+	 * Constructor that takes a string as argument.
+	 * 
+	 * @param sIn
+	 *            string that will be printed in a debug message
+	 */
+	public StringHolder(String sIn) {
+		System.out.println("Constructor with string argument, sIn = " + sIn
+				+ ", s = " + s + ".");
+	}
+
+}
