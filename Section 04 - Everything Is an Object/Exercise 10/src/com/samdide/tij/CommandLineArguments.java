caabdc5f15b40d28a129fdb7d6bfec7f27d3f098
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-05-27 22:38:48 +0200

    Commiting exercise 10.

---
+++ Section 04 - Everything Is an Object/Exercise 10/src/com/samdide/tij/CommandLineArguments.java
@@ -0,0 +1,24 @@
+package com.samdide.tij;
+
+/**
+ * Prints three arguments provided to the class on the command line.
+ * <p>
+ * Note that at least three arguments must be provided on the command line or
+ * the application will crash.
+ * 
+ * @author Henrik Samuelsson
+ */
+public class CommandLineArguments {
+	/**
+	 * Entry point to class and application.
+	 * 
+	 * @param args
+	 *            array of string arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+	}
+
+}
