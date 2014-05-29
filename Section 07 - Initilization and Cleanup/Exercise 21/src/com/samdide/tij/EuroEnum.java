a7bf7527a897b4c3ea9a7f1ac2e5e833f747cfd5
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-05-29 21:41:14 +0200

    Committing exercise 21.

---
+++ Section 07 - Initilization and Cleanup/Exercise 21/src/com/samdide/tij/EuroEnum.java
@@ -0,0 +1,28 @@
+package com.samdide.tij;
+
+/**
+ * Demo of an enumerated type.
+ * <p>
+ * Loops through and prints some values representing different euro bills.
+ * 
+ * @author Henrik Samuelsson
+ */
+public class EuroEnum {
+
+	public enum EuroBill {
+		FIVE, TEN, TWENTY, FIFTY, ONE_HUNDRED, TWO_HUNDRED
+	}
+
+	/**
+	 * Entry point to application.
+	 * 
+	 * @param args
+	 *            array of string arguments
+	 */
+	public static void main(String[] args) {
+		for (EuroBill e : EuroBill.values()) {
+			System.out.println(e + ", ordinal " + e.ordinal());
+		}
+	}
+
+}
